833bfee9f5f51ba47935fc0418c9d969512e76d0
Japneet <59381133+jassskalkat@users.noreply.github.com>
2023-03-03 00:59:00 -0400

    Add files via upload

---
+++ tenzies/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Press Start 2P"
+/>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
